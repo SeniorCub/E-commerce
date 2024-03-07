43c24cb60c080a3ae38cc7d3ec863fd21839fb49
seniorcub <farinderif@gmail.com>
2024-03-07 03:45:06 +0100

    Add dotenv package and update server port

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import path from 'path';
-
+import dotenv from 'dotenv';
 const app = express();
+dotenv.config();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use("/", express.static(path.join(process.cwd(), "/Public")))
 
 
-const PORT = (process.env.PORT || 9090 );
+const PORT = (process.env.PORT || 9998 );
 app.listen(PORT, ()=>{
      console.log(`Server running on http://localhost:${PORT}`);
 })

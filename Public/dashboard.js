80b295076de5338704c1d8392da47675f430153d
seniorcub <farinderif@gmail.com>
2024-03-09 07:32:16 +0100

    Added an atm look alike to display user account deatails

--- Public/dashboard.js
+++ Public/dashboard.js
@@ -4,3 +4,9 @@ document.querySelector('.offCavaBtn').addEventListener('click', ()=>{
     let extended = firstSide.style.display === "block";
     firstSide.style.display = extended ? "none" : "block";
 })
+document.querySelector('.masterCardIcon').addEventListener('click', ()=>{
+     
+     let thirdSide = document.querySelector('.thirdSide')
+    let extended = thirdSide.style.display === "block";
+    thirdSide.style.display = extended ? "none" : "block";
+})

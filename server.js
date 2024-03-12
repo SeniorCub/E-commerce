5a42ed0a4ec0640338e3543977505aa31b886521
seniorcub <farinderif@gmail.com>
2024-03-12 15:28:08 +0100

    Update package.json and server.js files

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 import express from 'express';
 import path from 'path';
+import bcrypt from 'bcrypt';
 import dotenv from 'dotenv';
+import connectDB from './dB/connect.js';
+import color from 'colors';
+import morgan from 'morgan';
+import usermodels from './models/usermodels.js';
+import authroute from './routes/authroute.js';
 const app = express();
 dotenv.config();
 
+connectDB()
+
 app.use(express.json());
+app.use(morgan("dev"))
 app.use(express.urlencoded({ extended: false }));
 app.use("/", express.static(path.join(process.cwd(), "/Public")))
-app.use("*", express.static(path.join(process.cwd(), "/View", "404.html")))
+// app.use("*", express.static(path.join(process.cwd(), "/View", "404.html")))
 
 
-const PORT = (process.env.PORT || 9998 );
+app.use('/api/v1/auth', authroute);
+
+const PORT = (process.env.PORT || 2020 );
 app.listen(PORT, ()=>{
-     console.log(`Server running on http://localhost:${PORT}`);
+     console.log(`Server running on http://localhost:${PORT}`.bgBlue.red);
 })

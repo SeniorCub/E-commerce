c8cde9bf30a08e570ebb5bf27a0961ce5e142eb4
seniorcub <farinderif@gmail.com>
2024-03-12 15:29:55 +0100

    Update user authentication logic

--- Public/products.js
+++ Public/products.js
@@ -13,6 +13,8 @@ let quantity = document.querySelector(".quantity");
 let deliver = document.querySelector(".deliver");
 let tax = document.querySelector(".tax");
 let maxtotal = document.querySelector(".maxtotal");
+let categoriesContainer = document.querySelector(".categories");
+let searchInput = document.querySelector(".form-control");
 
 // Declare a global variable to store the clicked category name
 var clickedCategoryName = '';
@@ -23,111 +25,111 @@ let totalProductsCount = 0;
 var listCards = [];
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Get the container that holds the product list
-    const productListContainer = document.querySelector('.list');
-
-    // Delegate the click event to the product list container
-    productListContainer.addEventListener('click', function (event) {
-        const addButton = event.target.closest('.addCart');
-        if (addButton) {
-            // Find the index of the product in the list
-            const key = addButton.dataset.key;
-            addToCart(event, key);
-        }
-    });
-
-    // Get all elements with the class 'eachCategory'
-    var categoryElements = document.querySelectorAll('.eachCategory');
-
-    // Iterate through each element and add a click event listener
-    categoryElements.forEach(function (element) {
-        element.addEventListener('click', function () {
-            // Get the inner text of the clicked category
-            clickedCategoryName = element.querySelector('.text').innerText;
-            console.log('Clicked category:', clickedCategoryName);
-
-            document.querySelector(".list").innerHTML = ``;
-            // Now you can use 'clickedCategoryName' to fetch data from an API or perform other actions.
-            fetchDataFromAPI(clickedCategoryName);
-        });
-    });
-
     // Fetch initial data for a default category (if needed)
     fetchDataFromAPI('Tv');
+
+    // Add event listener for search input
+    searchInput.addEventListener('input', function (event) {
+        searchProducts(event.target.value.trim().toLowerCase());
+    });
 });
 
 function fetchDataFromAPI(categoryName) {
     // Replace this URL with the actual path to your external JSON file
     var apiUrl = 'products.json';
 
-    // Store the current category globally
-    currentCategory = categoryName;
-
     // Fetch data from the API
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
             fetchedData = data; // Store fetched data globally
-            // Check if the category exists in the JSON data
-            if (categoryName in fetchedData) {
-                var categoryData = fetchedData[categoryName];
 
-               // Update the totalProductsCount variable
-               totalProductsCount = categoryData.length;
+            // Get all unique category names
+            const uniqueCategories = [...new Set(Object.keys(fetchedData))];
+            // Display categories in the tooltip container
+            displayCategories(uniqueCategories);
 
-                // Create a container div for the products
-                let categoryContainer = document.createElement("div");
-                categoryContainer.classList.add("category-container");
-
-                categoryData.forEach((value, key) => {
-                    // Add a quantity property to each product
-                    value.quantity = 0;
-
-                    let newDiv = document.createElement("div");
-                    newDiv.classList.add("item");
-                    newDiv.innerHTML = `
-                        <div class="card">
-                            <div class="card-title d-flex">
-                                <div class="btn btn-pri percent">${value.rating.rate}</div>
-                                <div class="btn btn-pri love"><i class="fa-regular fa-heart" style="color: #58A0E2;"></i></div>
-                            </div>
-
-                            <img src=${value.image} alt="product" width="100px">
-
-                            <div class="card-title d-flex">
-                                <div class="btn btn-sm btn-pri cartgo">${value.category}</div>
-                                <h5 class="original">$${value.price.toLocaleString()}</h5>
-                            </div>
-
-                            <p class="card-text">${value.name}</p>
-
-                            <button class="CartBtn addCart" data-key="${key}">
-                                <span class="IconContainer"> 
-                                    <i class="fa-solid fa-cart-shopping" style="color: #ffffff;"></i>
-                                </span>
-                                <span class="text">Add to Cart</span>
-                            </button>
-                        </div>
-                    `;
-                    categoryContainer.appendChild(newDiv);
-                });
-
-                // Append the container with products to the main list
-                list.appendChild(categoryContainer);
-            } else {
-                console.error('Category not found:', categoryName);
-            }
             // Display the total number of products on the webpage
+            totalProductsCount = fetchedData[categoryName].length;
             displayTotalProductsCount(totalProductsCount);
+
+            // Update the current category globally
+            currentCategory = categoryName;
+
+            // Create a container div for the products
+            let categoryContainer = document.createElement("div");
+            categoryContainer.classList.add("category-container");
+
+            fetchedData[categoryName].forEach((value, key) => {
+                // Add a quantity property to each product
+                value.quantity = 0;
+
+                let newDiv = document.createElement("div");
+                newDiv.classList.add("item");
+                newDiv.innerHTML = `
+                    <div class="card">
+                        <div class="card-title d-flex">
+                            <div class="btn btn-pri percent">${value.rating.rate}</div>
+                            <div class="btn btn-pri love"><i class="fa-regular fa-heart" style="color: #58A0E2;"></i></div>
+                        </div>
+
+                        <img src=${value.image} alt="product" width="100px">
+
+                        <div class="card-title d-flex">
+                            <div class="btn btn-sm btn-pri cartgo">${value.category}</div>
+                            <h5 class="original">$${value.price.toLocaleString()}</h5>
+                        </div>
+
+                        <p class="card-text">${value.name}</p>
+
+                        <button class="CartBtn addCart" data-key="${key}">
+                            <span class="IconContainer"> 
+                                <i class="fa-solid fa-cart-shopping" style="color: #ffffff;"></i>
+                            </span>
+                            <span class="text">Add to Cart</span>
+                        </button>
+                    </div>
+                `;
+                categoryContainer.appendChild(newDiv);
+            });
+
+            // Append the container with products to the main list
+            list.appendChild(categoryContainer);
         })
         .catch(error => {
             console.error('Error fetching data:', error);
         });
+}
 
-        function displayTotalProductsCount(count) {
-          // Display the count on the webpage (update the element ID accordingly)
-          document.getElementById('totalProductsCount').innerText = count;
-      }
+function displayCategories(categories) {
+    // Clear existing categories
+    categoriesContainer.innerHTML = '';
+
+    // Append each category to the container
+    categories.forEach(category => {
+        let categoryDiv = document.createElement("div");
+        categoryDiv.classList.add("eachCategory", "tooltip-container");
+        categoryDiv.innerHTML = `
+            <div class="text">${category}</div>
+            <div class="tooltip">
+                <img src="images/${category.toLowerCase()}.png" alt="Tooltip Image">
+            </div>
+        `;
+        categoryDiv.addEventListener('click', function () {
+            clickedCategoryName = category;
+            console.log('Clicked category:', clickedCategoryName);
+
+            document.querySelector(".list").innerHTML = ``;
+            fetchDataFromAPI(clickedCategoryName);
+        });
+
+        categoriesContainer.appendChild(categoryDiv);
+    });
+}
+
+function displayTotalProductsCount(count) {
+    // Display the count on the webpage (update the element ID accordingly)
+    document.getElementById('totalProductsCount').innerText = count;
 }
 
 function addToCart(event, key) {
@@ -207,3 +209,73 @@ function changeQuantity(key, quantity) {
     fetchedData[currentCategory][key].quantity = quantity;
     reloadCard();
 }
+document.addEventListener('DOMContentLoaded', function () {
+     // Get the container that holds the product list
+     const productListContainer = document.querySelector('.list');
+ 
+     // Get the search input element
+     const searchInput = document.querySelector('.form-control');
+ 
+     // Delegate the click event to the product list container
+     productListContainer.addEventListener('click', function (event) {
+         const addButton = event.target.closest('.addCart');
+         if (addButton) {
+             // Find the index of the product in the list
+             const key = addButton.dataset.key;
+             addToCart(event, key);
+         }
+     });
+ 
+     // Add event listener to search input for keydown event
+     searchInput.addEventListener('input', function(event) {
+         const query = event.target.value.toLowerCase();
+         searchProducts(query);
+     });
+ 
+     // Fetch initial data for a default category (if needed)
+     fetchDataFromAPI('Tv');
+ });
+ 
+ function searchProducts(query) {
+     const allProducts = Object.values(fetchedData).flat();
+     const filteredProducts = allProducts.filter(product => {
+         return product.name.toLowerCase().includes(query);
+     });
+ 
+     list.innerHTML = '';
+ 
+     let categoryContainer = document.createElement("div");
+     categoryContainer.classList.add("category-container");
+ 
+     filteredProducts.forEach(product => {
+         let newDiv = document.createElement("div");
+         newDiv.classList.add("item");
+         newDiv.innerHTML = `
+             <div class="card">
+                 <div class="card-title d-flex">
+                     <div class="btn btn-pri percent">${product.rating.rate}</div>
+                     <div class="btn btn-pri love"><i class="fa-regular fa-heart" style="color: #58A0E2;"></i></div>
+                 </div>
+ 
+                 <img src=${product.image} alt="product" width="100px">
+ 
+                 <div class="card-title d-flex">
+                     <div class="btn btn-sm btn-pri cartgo">${product.category}</div>
+                     <h5 class="original">$${product.price.toLocaleString()}</h5>
+                 </div>
+ 
+                 <p class="card-text">${product.name}</p>
+ 
+                 <button class="CartBtn addCart" data-key="${product.key}">
+                     <span class="IconContainer"> 
+                         <i class="fa-solid fa-cart-shopping" style="color: #ffffff;"></i>
+                     </span>
+                     <span class="text">Add to Cart</span>
+                 </button>
+             </div>
+         `;
+         categoryContainer.appendChild(newDiv);
+     });
+ 
+     list.appendChild(categoryContainer);
+ } 

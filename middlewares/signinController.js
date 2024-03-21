c6c5d6663507ab1b26ab5d092aafcdce9272f405
Reuben Ifeoluwa Farinde <99650027+SeniorCub@users.noreply.github.com>
2024-03-21 13:42:11 +0000

    update

--- middlewares/signinController.js
+++ middlewares/signinController.js
@@ -7,7 +7,7 @@ dotenv.config();
 export const signinController = async (req, res, next) => {
      try {
           const authHeader = req.headers.authorization
-          const token = authHeader.split(' ')[1];
+          const token = authHeader.split(' ')[1]
           const decode = await jwt.verify(token, process.env.JWT_SECRET)
           req.user = decode
           next()
@@ -22,8 +22,13 @@ export const isAdmin = async  (req, res, next) => {
           if (user.role !== 1) {
                return res.status(201).send({
                     success: false,
-                    msg: 'Unauthorized User'
-               })
+                    msg: "Unauthorized user",
+                    user: {
+                        name: user.name,
+                        email: user.email,
+                        role: user.role
+                    }
+                })
           } else {
                // When Using Postman
                return res.status(201).send({

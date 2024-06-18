4298dea07b632f3da51035f7bc3b8f9a178da726
SeniorCub <farinderif@gmail.com>
2024-06-18 17:44:03 +0100

    Update login page and dashboard functionality

--- Public/dashboard.js
+++ Public/dashboard.js
@@ -1,20 +1,27 @@
 document.querySelector('.offCavaBtn').addEventListener('click', ()=>{
-     
      let firstSide = document.querySelector('.firstSide')
     let extended = firstSide.style.display === "block";
     firstSide.style.display = extended ? "none" : "block";
 })
 document.querySelector('.masterCardIcon').addEventListener('click', ()=>{
-     
      let thirdSide = document.querySelector('.thirdSide')
     let extended = thirdSide.style.display === "block";
     thirdSide.style.display = extended ? "none" : "block";
 })
 document.querySelectorAll('.recentOrderOptions').forEach((option)=>{
      option.addEventListener('click', ()=>{
-     
           let optionsss = document.querySelector('.optionsss')
          let extended = optionsss.style.display === "block";
          optionsss.style.display = extended ? "none" : "block";
      })
+})
+document.querySelector('.calculatorHtml').addEventListener('click', ()=>{
+     let calculator = document.querySelector('.calculator')
+    let extended = calculator.style.display === "block";
+    calculator.style.display = extended ? "none" : "block";
+})
+document.querySelector('.close').addEventListener('click', ()=>{
+     let calculator = document.querySelector('.calculator')
+    let extended = calculator.style.display === "block";
+    calculator.style.display = extended ? "none" : "block";
 })
